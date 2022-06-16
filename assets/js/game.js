e6b6242006ff2d88f21073f42061c7fefe0679ed
Wavelopper <alnetweb59@gmail.com>
2022-06-16 15:08:10 +0200

    [UPDATE]

---
+++ assets/js/game.js
@@ -0,0 +1,19 @@
+/* Création d'une classe appelée Game. */
+export default class Game {
+  constructor() {
+    this.params = {
+      
+    };
+
+    this.init();
+  }
+
+  /* Une fonction qui est appelée lorsque la page est chargée. */
+  init = () => {
+    window.addEventListener("load", () => {
+      
+    });
+  };
+}
+  
+const game = new Game();
